df75b34c5f942307ac80c75989b0555c0e0a9aec
Anton Zmanovskii <antonzm34@gmail.com>
2020-09-18 14:26:01 +0400

    contacts

---
+++ components/map.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="contacts__map-container">
+    <div v-if="overlay" class="map__overlay" @click="overlayToggler"></div>
+    <script
+      type="text/javascript"
+      charset="utf-8"
+      async
+      src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9b0c838fc9effbe8aec1af35eae45eaf3b9309f3734b9a6b8716d256728caf36&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"
+    ></script>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    overlayToggler() {
+      this.overlay = !this.overlay;
+    },
+  },
+  data() {
+    return {
+      overlay: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.contacts__map-container {
+  position: relative;
+  width: 100%;
+  height: 400px;
+}
+.map__overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  cursor: pointer;
+}
+</style>
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,45 +1,94 @@
 <template>
-  <div>
-    <content-box class="contacts__content">
-      <content-title :title="'Контакты'" />
-      <div class="contacts__item">
-        <img class="contact-icon" src="../static/mail.png" alt="" />
-        <p class="contacts__text">400081, г.Волгоград, ул.Ангарская 69а</p>
-      </div>
-      <div class="contacts__item">
-        <img class="contact-icon" src="../static/mail.png" alt="" />
-        <p class="contacts__text">(8442) 98-36-73</p>
-      </div>
-      <div class="contacts__item">
-        <img class="contact-icon" src="../static/mail.png" alt="" />
-        <p class="contacts__text">info@stvorka34.ru</p>
-      </div>
-    </content-box>
-    <script
-      type="text/javascript"
-      charset="utf-8"
-      async
-      src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9b0c838fc9effbe8aec1af35eae45eaf3b9309f3734b9a6b8716d256728caf36&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"
-    ></script>
+  <div class="contacts">
+    <content-title class="contacts__title" :title="'Контакты'" />
+
+    <div class="contacts__wrapper">
+      <content-box class="contacts__content">
+        <div class="contacts__item">
+          <img class="contact-icon" src="../static/mail.png" alt="" />
+          <p class="contacts__text">
+            400081, г.Волгоград,<br />
+            ул.Ангарская 69а
+          </p>
+        </div>
+        <div class="contacts__item">
+          <img class="contact-icon" src="../static/phone.png" alt="" />
+          <p class="contacts__text">(8442) 98-36-73</p>
+          <p class="contacts__text">8-917-338-36-73</p>
+        </div>
+        <div class="contacts__item">
+          <img class="contact-icon" src="../static/email.png" alt="" />
+          <p class="contacts__text">info@stvorka34.ru</p>
+        </div>
+        <div class="contacts__item">
+          <img class="contact-icon" src="../static/social.png" alt="" />
+          <p class="contacts__text" id="tlg"><img class="social-icon" src="../static/tlg.png" alt="Telegram" />@stvorka34office</p>
+          <p class="contacts__text" id="inst"><img class="social-icon" src="../static/insta.png" alt="Instagram" />@okna_volgograd34</p>
+          <p class="contacts__text" id="wa"><img class="social-icon" src="../static/wa.png" alt="WhatsApp" />89173383673</p>
+          <p class="contacts__text" id="viber"><img class="social-icon" src="../static/viber.png" alt="Viber" />89173383673</p>
+        </div>
+      </content-box>
+    </div>
+    <ya-map />
   </div>
 </template>
 
 <script>
 import Content from '@/components/content';
 import ContentTitle from '@/components/content-title';
+import Map from '@/components/map';
 export default {
   components: {
     'content-box': Content,
     'content-title': ContentTitle,
+    'ya-map': Map,
   },
 };
 </script>
 
 <style scoped>
-.contacts__content {
-  padding: 60px;
+.contacts {
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-top: 60px;
+}
+.contacts__content {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  justify-content: space-between;
+}
+
+.contacts__wrapper {
+  margin-top: 50px;
+  padding-bottom: 50px;
+  width: 100%;
+}
+.contacts__title {
+  margin: 0 auto;
+  text-align: center;
+}
+.contacts__item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+}
+
+.contacts__text {
+  text-align: center;
+  font-family: Inter, Arial, Helvetica, sans-serif;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  color: #635e5e;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.social-icon {
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
 }
 </style>
