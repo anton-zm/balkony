e9f5f471b4e5979f67be748c411895356d6f1cf5
AntonZ <antonzm34@gmail.com>
2021-02-14 16:54:25 +0300

    paths tlg3

--- components/footer.vue
+++ components/footer.vue
@@ -4,7 +4,7 @@
     <line-decor class="footer__line" />
     <div class="footer__social">
       <a href="https://www.instagram.com/okna_volgograd34/" class="footer__social-link"><img src="../static/insta.png" alt="Instagram" class="footer__social-img"/></a>
-      <a href="https://t.me/stvorka34" class="footer__social-link"><img src="../static/tlg.png" alt="Telegram" class="footer__social-img"/></a>
+      <a href="https://t.me/stvorka34" class="footer__social-link"><img src="../static/telegram.png" alt="Telegram" class="footer__social-img"/></a>
       <a href="https://vk.com/stvorka34" class="footer__social-link"><img src="../static/vk.png" alt="VK" class="footer__social-img"/></a>
       <a href="http://wa.me/+79173383673" class="footer__social-link"><img src="../static/wa.png" alt="WhatsApp" class="footer__social-img"/></a>
     </div>
